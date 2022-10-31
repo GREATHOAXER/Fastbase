3436a1252692a7dd6f4c57fcf178f0769e7e429c
GREATHOAXER <shorelinesquare@naver.com>
2022-10-31 17:39:10 +0900

    :sparkles: Initialized uikit programmatically
    Fixed xcode project with UIKit programmatical way, and added test label

--- fastbase/fastbase/ViewController.swift
+++ fastbase/fastbase/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name : "Pretendard-bold", size: 14)
+        label.text = "TEST"
+        label.textColor = .black
+        label.layer.borderWidth = 1
+        label.textAlignment = .center
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.widthAnchor.constraint(equalTo: view.widthAnchor),
+            label.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 
 
